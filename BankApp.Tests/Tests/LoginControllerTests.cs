1dfdb3ba2b800f185acaec68208bf4b9bb3e4a68
Dimitry Sukalo <dimitrysukalo@gmail.com>
2020-08-21 19:12:24 +0300

    Добавлена html-форма входа в аккаунт

---
+++ BankApp.Tests/Tests/LoginControllerTests.cs
@@ -0,0 +1,23 @@
+﻿using BankApp.WEB.Controllers;
+using Microsoft.AspNetCore.Mvc;
+using Xunit;
+
+namespace BankApp.Tests.Tests
+{
+    public class LoginControllerTests
+    {
+        [Fact]
+        private void IndexViewTests()
+        {
+            //Arrange
+            var loginController = new LoginController();
+
+            //Act
+            var result = loginController.Index() as ViewResult;
+
+            //Assert
+            Assert.NotNull(result);
+            Assert.Equal("Login", result.ViewName);
+        }
+    }
+}
---
+++ BankApp.WEB/Controllers/LoginController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BankApp.WEB.Controllers
+{
+    public class LoginController : Controller
+    {
+        /// <summary>
+        /// Login form
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View("Login");
+        }
+    }
+}
