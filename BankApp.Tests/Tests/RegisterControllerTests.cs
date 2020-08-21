a2a76c4ca17c624e930f37c6d09aaf8d39b792f5
Dimitry Sukalo <dimitrysukalo@gmail.com>
2020-08-21 19:01:01 +0300

    Добавлена форма регистрации клиента, но еще логического функционала нету

---
+++ BankApp.Tests/Tests/RegisterControllerTests.cs
@@ -0,0 +1,23 @@
+﻿using BankApp.WEB.Controllers;
+using Microsoft.AspNetCore.Mvc;
+using Xunit;
+
+namespace BankApp.Tests.Tests
+{
+    public class RegisterControllerTests
+    {
+        [Fact]
+        private void IndexViewTests()
+        {
+            //Arrange
+            var registerController = new RegisterController();
+
+            //Act
+            var result = registerController.Index() as ViewResult;
+
+            //Assert
+            Assert.NotNull(result);
+            Assert.Equal("Register", result?.ViewName);
+        }
+    }
+}
---
+++ BankApp.WEB/Controllers/RegisterController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BankApp.WEB.Controllers
+{
+    public class RegisterController : Controller
+    {
+        /// <summary>
+        /// Register form
+        /// </summary>
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View("Register");
+        }
+    }
+}
